62b65970c9d3e111426a48f8cfd9162b3bab0ef0
Jonah Dempcy <jdempcy@gmail.com>
2022-10-27 19:07:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SFHDL
+### The Santa Fe Human Design Library
+
+
+Based on `https://github.com/hfpp2012/railstart-niceadmin`. (Now defunct.)
+
+### Quickstart
+```
+bundle install
+rails db:migrate
+rails db:seed
+```
+
+### To run the app:
+
+```
+rails server
+```
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+require 'sidekiq/web'
+Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
+  root 'static_pages#home'
+
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
+  mount Sidekiq::Web => '/sidekiq'
+  mount PgHero::Engine, at: 'pghero'
+
+  draw(:admin)
+
+  %w(404 422 500).each do |code|
+    get code, to: 'errors#show', code: code
+  end
+end
